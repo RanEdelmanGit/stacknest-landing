cb22a127090b6ad1e3093ae824c8b06d2d987a54
RanEdelmanGit <ranedelmann@gmail.com>
2024-02-10 19:00:36 +0200

    initial version

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'blue': '#2D2879',
+        'purple': '#9091DC',
+      }
+    },
+  },
+  plugins: [],
+}
+
