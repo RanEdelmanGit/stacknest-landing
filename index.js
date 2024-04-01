9d87368c1aa39ccb4c72c52cc8c2e5e79317838d
RanEdelmanGit <ranedelmann@gmail.com>
2024-04-01 12:41:01 +0300

    nav burger ready

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-function toggleMenu() {
-    var x = document.querySelector(".topnav > div");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const hamMenu = document.querySelector('.ham-menu');
+//   const menu = document.querySelector('.menu');
+
+//   hamMenu.addEventListener('click', () => {
+//     // The menu is hidden by default on small screens, so we toggle it
+//     // const isMdScreen = window.matchMedia('(max-width: 768px)').matches; 
+//     const isMenuHidden = menu.classList.contains('hidden');
+//     if (isMenuHidden) {
+//       menu.classList.remove('hidden');
+//       menu.classList.add('max-md:flex-col');
+//       console.log('menu is hidden');
+//     } else {
+//       menu.classList.remove('max-md:flex');
+//       menu.classList.add('hidden');
+//       console.log('menu is shown');
+//     }
+//   });
+// });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const hamMenu = document.querySelector('.ham-menu');
+  const menu = document.querySelector('.menu');
+
+  hamMenu.addEventListener('click', () => {
+    // Toggle menu visibility on small screens
+    const isMenuHidden = menu.classList.contains('hidden');
+    if (isMenuHidden) {
+      menu.classList.remove('hidden');
+      console.log('Menu is now shown');
     } else {
-      x.style.display = "flex";
+      menu.classList.add('hidden');
+      console.log('Menu is now hidden');
     }
-  }
-  
+  });
+});
+
