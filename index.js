a772e0b557c2b0b431dbef6eb6fc337761b1ada8
RanEdelmanGit <ranedelmann@gmail.com>
2024-04-04 22:46:48 +0300

    last

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   const hamMenu = document.querySelector('.ham-menu');
-//   const menu = document.querySelector('.menu');
-
-//   hamMenu.addEventListener('click', () => {
-//     // The menu is hidden by default on small screens, so we toggle it
-//     // const isMdScreen = window.matchMedia('(max-width: 768px)').matches; 
-//     const isMenuHidden = menu.classList.contains('hidden');
-//     if (isMenuHidden) {
-//       menu.classList.remove('hidden');
-//       menu.classList.add('max-md:flex-col');
-//       console.log('menu is hidden');
-//     } else {
-//       menu.classList.remove('max-md:flex');
-//       menu.classList.add('hidden');
-//       console.log('menu is shown');
-//     }
-//   });
-// });
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
   const hamMenu = document.querySelector('.ham-menu');
   const menu = document.querySelector('.menu');
@@ -29,10 +7,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const isMenuHidden = menu.classList.contains('hidden');
     if (isMenuHidden) {
       menu.classList.remove('hidden');
-      console.log('Menu is now shown');
     } else {
       menu.classList.add('hidden');
-      console.log('Menu is now hidden');
     }
   });
 });
